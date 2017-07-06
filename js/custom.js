25f849543349c8dc9ec9679a323d4fba4cf98a26
raghibnoor <caesar.rpmc@gmail.com>
2017-07-06 08:21:01 +0600

    initial1

---
+++ js/custom.js
@@ -0,0 +1,80 @@
+ $(document).ready(function () { // 6,32 5,38 2,34
+
+
+
+     $("#caesar-circle-1").circliful({
+         animation: 1,
+         animationStep: 6,
+         foregroundBorderWidth: 15,
+         backgroundBorderWidth: 15,
+         percent: 90,
+         iconColor: '#3498DB',
+         icon: 'f109',
+         iconSize: '30',
+         iconPosition: 'bottom',
+         foregroundColor: "#30BAE7",
+         backgroundColor: "#DFE8ED",
+         percentageY: 90,
+         animateInView: true,
+
+
+     });
+
+     $("#caesar-circle-2").circliful({
+         animation: 1,
+         animationStep: 6,
+         foregroundBorderWidth: 15,
+         backgroundBorderWidth: 15,
+         percent: 70,
+         iconColor: '#D74680',
+         icon: 'f13b',
+         iconSize: '30',
+         iconPosition: 'bottom',
+         backgroundColor: "#DFE8ED",
+         foregroundColor: "#D74680",
+         percentageY: 90,
+         animateInView: true,
+
+
+     });
+
+     $("#caesar-circle-3").circliful({
+         animation: 1,
+         animationStep: 6,
+         foregroundBorderWidth: 15,
+         backgroundBorderWidth: 15,
+         backgroundColor: '#000',
+         foregroundColor: '#A8C64A',
+         fontColor: '#000',
+         iconColor: '#15C7A8',
+         icon: 'f108',
+         iconSize: '25',
+         iconPosition: 'bottom',
+         backgroundColor: "#DFE8ED",
+         foregroundColor: "#15C7A8",
+         percent: 75,
+         percentageY: 80,
+         animateInView: true,
+
+
+     });
+
+     $("#caesar-circle-4").circliful({
+         animation: 1,
+         animationStep: 6,
+         foregroundBorderWidth: 15,
+         backgroundBorderWidth: 15,
+         percent: 85,
+         icon: 'f2bd',
+         iconSize: '25',
+         iconColor: '#EB7D4B',
+         iconPosition: 'bottom',
+         textBelow: true,
+         backgroundColor: "#DFE8ED",
+         foregroundColor: "#EB7D4B",
+         percentageY: 80,
+         animateInView: true,
+
+
+     });
+ });
